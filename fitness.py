898b8a12e84e506a2c86b7cb4186efdbe5ebeff0
IVequals4 <isaac.velayo@gmail.com>
2023-12-14 21:15:40 -0800

    Update fitness.py: Checks for steps in the LLM answer

--- fitness.py
+++ fitness.py
@@ -10,8 +10,46 @@ def check_answer(answer, output):
     print("answer is wrong")
     return -1
 
+
+# finds the logical steps from database answer
 def get_logic(answer):
     return re.findall("<<(.*?)>>", answer)
+
+
+# splits database answer into tokens
+def tokenise_logic(logic):
+    tokenised_logic = []
+    operators = "+-/*="
+    current_token = ""
+    
+    for c in logic:
+        if c in operators:
+            tokenised_logic.append(current_token)
+            tokenised_logic.append(c)
+            current_token = ""
+        elif c == '.' or c.isnumeric():
+            current_token += c
+    tokenised_logic.append(current_token)
+    
+    return tokenised_logic
+
+
+# checks to see if the database answer is in the line
+def check_logic_sentence(line, logic_tokens):
+    i = 0
+
+    for word in line.split(" "):
+        # print(f'{word} compared to {logic_tokens[i]} at i = {i}')
+        if word == logic_tokens[i]:
+            i += 1
+        else:
+            if i > 0:
+                i = 0
+        if i == len(logic_tokens)-1:
+            return True
+        
+    return False
+            
 
 if __name__ == '__main__':
     database_answer = 'Natalia sold 48/2 = <<48/2=24>>24 clips in May. Natalia sold 48+24 = <<48+24=72>>72 clips altogether in April and May. #### 72'
@@ -24,13 +62,20 @@ if __name__ == '__main__':
     
     logic = get_logic(database_answer)
     print(logic)
+    
+    tokenised_logic_sentences = []
+    
+    for l in logic:
+        tokenised_logic_sentences.append(tokenise_logic(l))
 
     with open(llm_answer_file, 'r') as f:
         for line in f:
-            pass
+            for sentence in tokenised_logic_sentences:
+                if check_logic_sentence(line, sentence):
+                    score += 1
+                    break
         last_line = line
 
-    print(last_line)
     for word in last_line.split(" "):
         try:
             num_answer = str(int(word))
@@ -38,5 +83,5 @@ if __name__ == '__main__':
             pass
         
     score += check_answer(num_answer, last_line)
-    
+    print(score)
     
