81c76c30427b63c63f60a81a2f3349207ac7fe89
Kerui Yang <kerui.yang@outlook.com>
2023-12-21 09:31:53 +1100

    added instructions to modify places

--- genetic_server.py
+++ genetic_server.py
@@ -4,13 +4,15 @@ Challenge 3: Genetic Algorithm for Automated Prompt Engineering
 """
 import os
 
+
+#-e git+https://github.com/hypercycle-development/pyhypercycle-aim.git#egg=pyhypercycle_aim
+
 from pyhypercycle_aim import SimpleServer, JSONResponseCORS, aim_uri
 
 from genetic_algorithm import GeneticAlgorithm
 from prompt_objective import PromptObjective
 
 PORT = os.environ.get("PORT", 4002)
-
 
 class GeneticExample(SimpleServer):
     manifest = {"name": "GeneticExample",
@@ -28,16 +30,18 @@ class GeneticExample(SimpleServer):
                  "input_query": "",
                  "input_headers": "",
                  "output": {},
-                 "documentation": "Returns the prompt and the score based on the desired output",
+                 "documentation": "Returns the prompt and the score based on the desired output", # REPLACE
                  "example_calls": [{
                      "body": {"target_output": "simple, lively, strong"},
                      "method": "POST",
                      "query": "",
                      "headers": "",
-                     "output": {"prompt": "simple, lively, strong", "score": 0.004}
+                     "output": {"prompt": "simple, lively, strong"}
                  }]
              })
     async def prompt(self, request):
+
+        # REPLACE WITH OUR OWN FUNCTION
         # define the total iterations
         n_iter = 100
         # bits
@@ -61,6 +65,7 @@ class GeneticExample(SimpleServer):
                                                                   r_cross,
                                                                   r_mut)
         best = pa.phenotype(best_genotype)
+        #REPLACE
         return JSONResponseCORS({"prompt": best, "score": score})
 
 
